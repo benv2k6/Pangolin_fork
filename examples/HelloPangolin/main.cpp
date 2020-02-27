76ff8e28cea953a77d335840c98663f8ef576a8f
ben <ben.weisbich@r-go.io>
2020-02-27 12:37:06 +0200

    better rotations

--- examples/HelloPangolin/main.cpp
+++ examples/HelloPangolin/main.cpp
@@ -2,11 +2,16 @@
 #include <Eigen/Geometry>
 #include <Eigen/SVD>
 #include <chrono>
+// Converts degrees to radians.
+#define degreesToRadians(angleDegrees) (angleDegrees * M_PI / 180.0)
+
+// Converts radians to degrees.
+#define radiansToDegrees(angleRadians) (angleRadians * 180.0 / M_PI)
+
 class RgoHandler3d : public pangolin::Handler3D {
    public:
     RgoHandler3d::RgoHandler3d(pangolin::OpenGlRenderState& cam_state)
-        : pangolin::Handler3D(cam_state) {
-        }
+        : pangolin::Handler3D(cam_state) {}
     void Keyboard(pangolin::View& view, unsigned char key, int x, int y,
                   bool pressed) {
         if (!pressed) {
@@ -16,29 +21,82 @@ class RgoHandler3d : public pangolin::Handler3D {
         Eigen::Matrix4d transform;
         transform.setIdentity();
         const Eigen::Matrix4d mvmat = cam_state->GetModelViewMatrix();
-        const Eigen::Matrix3d R_env_cam =
-            mvmat.block<3, 3>(0, 0).cast<double>().transpose();
+        const Eigen::Matrix3d R_env_cam = mvmat.block<3, 3>(0, 0).transpose();
         switch (key) {
             case pangolin::PANGO_SPECIAL + pangolin::PANGO_KEY_LEFT:
-            transform.block<3, 1>(0, 3) = R_env_cam * unit_vec_left_* move_factor_;
-            break;
+                transform.block<3, 1>(0, 3) =
+                    R_env_cam * unit_vec_left_ * move_factor_;
+                break;
             case pangolin::PANGO_SPECIAL + pangolin::PANGO_KEY_RIGHT:
-            transform.block<3, 1>(0, 3) = -R_env_cam * unit_vec_left_* move_factor_;
-            break;
+                transform.block<3, 1>(0, 3) =
+                    -R_env_cam * unit_vec_left_ * move_factor_;
+                break;
             case pangolin::PANGO_SPECIAL + pangolin::PANGO_KEY_UP:
-            transform.block<3, 1>(0, 3) = R_env_cam * unit_vec_forward_* move_factor_;
-            break;
+                transform.block<3, 1>(0, 3) =
+                    R_env_cam * unit_vec_forward_ * move_factor_;
+                break;
             case pangolin::PANGO_SPECIAL + pangolin::PANGO_KEY_DOWN:
-            transform.block<3, 1>(0, 3) = -R_env_cam * unit_vec_forward_* move_factor_;
-            break;
+                transform.block<3, 1>(0, 3) =
+                    -R_env_cam * unit_vec_forward_ * move_factor_;
+                break;
+            case 'a':
+                transform.block<3, 3>(0, 0) =
+                    Eigen::Quaternion<double>(R_env_cam).matrix();
+                break;
+            case 'd':
+                transform.block<3, 3>(0, 0) =
+                    Eigen::Quaternion<double>(
+                        R_env_cam *
+                        Eigen::AngleAxis<double>(degreesToRadians(90),
+                                                 Eigen::Vector3d::UnitX()))
+                        .matrix();
+                break;
+            case 'x':
+                transform.block<3, 3>(0, 0) =
+                    Eigen::Quaternion<double>(
+                        R_env_cam *
+                        Eigen::AngleAxis<double>(degreesToRadians(90),
+                                                 Eigen::Vector3d::UnitY()))
+                        .matrix();
+                break;
+            case 'q':
+                transform.block<3, 3>(0, 0) =
+                    Eigen::AngleAxis<double>(
+                        degreesToRadians(-rotation_factor_deg_),
+                        (R_env_cam * unit_vec_forward_).normalized())
+                        .matrix();
+                break;
+            case 'e':
+                transform.block<3, 3>(0, 0) =
+                    Eigen::AngleAxis<double>(
+                        degreesToRadians(rotation_factor_deg_),
+                        (R_env_cam * unit_vec_forward_).normalized())
+                        .matrix();
+                break;
+            case 'w':
+                transform.block<3, 3>(0, 0) =
+                    Eigen::AngleAxis<double>(
+                        degreesToRadians(-rotation_factor_deg_),
+                        (R_env_cam * unit_vec_left_).normalized())
+                        .matrix();
+                break;
+            case 's':
+                transform.block<3, 3>(0, 0) =
+                    Eigen::AngleAxis<double>(
+                        degreesToRadians(rotation_factor_deg_),
+                        (R_env_cam * unit_vec_left_).normalized())
+                        .matrix();
+                break;
         }
-        cam_state->SetModelViewMatrix(static_cast<Eigen::Matrix4d>(mvmat * transform));
+        cam_state->SetModelViewMatrix(
+            static_cast<Eigen::Matrix4d>(mvmat * transform));
     }
 
    private:
     Eigen::Vector3d unit_vec_forward_{0, 0, 1};
     Eigen::Vector3d unit_vec_left_{1, 0, 0};
     float move_factor_ = 0.4f;
+    float rotation_factor_deg_ = 5.f;
 };
 
 int main(int /*argc*/, char** /*argv*/) {
@@ -50,16 +108,16 @@ int main(int /*argc*/, char** /*argv*/) {
         pangolin::ProjectionMatrix(640, 480, 420, 420, 320, 240, 0.2, 100),
         pangolin::ModelViewLookAt(-12, 2, -12, 0, 0, 0, pangolin::AxisY));
 
-    bool axis_align = false;
-    pangolin::RegisterKeyPressCallback('q',
-                                       [&]() { axis_align = !axis_align; });
+    // bool axis_align = false;
+    // pangolin::RegisterKeyPressCallback('q',
+    //                                    [&]() { axis_align = !axis_align; });
     // pangolin::RegisterKeyPressCallback('w', [&](){show_x0 = !show_x0;});
     // pangolin::RegisterKeyPressCallback('e', [&](){show_y0 = !show_y0;});
     // pangolin::RegisterKeyPressCallback('r', [&](){show_z0 = !show_z0;});
 
     // Create Interactive View in window
     pangolin::Handler3D handler(s_cam);
-    const int UI_WIDTH = 180;
+    const int UI_WIDTH = 0;
     pangolin::View& d_cam =
         pangolin::CreateDisplay()
             .SetBounds(0.0, 1.0, pangolin::Attach::Pix(UI_WIDTH), 1.0,
@@ -67,14 +125,14 @@ int main(int /*argc*/, char** /*argv*/) {
             .SetHandler(new RgoHandler3d(s_cam));
 
     // A Panel is just a View with a default layout and input handling
-    pangolin::CreatePanel("ui").SetBounds(0.0, 1.0, 0.0,
-                                          pangolin::Attach::Pix(UI_WIDTH));
-    pangolin::Var<double> center_x("ui.center z", 0, -30, 30);
-    pangolin::Var<double> center_y("ui.center y", 0, -30, 30);
-    pangolin::Var<double> center_z("ui.center x", 0, -30, 30);
-    pangolin::Var<double> view_x("ui.view x", 1, -30, 30);
-    pangolin::Var<double> view_y("ui.view y", -2, -30, 30);
-    pangolin::Var<double> view_z("ui.view z", 1, -30, 30);
+    // pangolin::CreatePanel("ui").SetBounds(0.0, 1.0, 0.0,
+    //                                       pangolin::Attach::Pix(UI_WIDTH));
+    // pangolin::Var<double> center_x("ui.center z", 0, -30, 30);
+    // pangolin::Var<double> center_y("ui.center y", 0, -30, 30);
+    // pangolin::Var<double> center_z("ui.center x", 0, -30, 30);
+    // pangolin::Var<double> view_x("ui.view x", 1, -30, 30);
+    // pangolin::Var<double> view_y("ui.view y", -2, -30, 30);
+    // pangolin::Var<double> view_z("ui.view z", 1, -30, 30);
 
     while (!pangolin::ShouldQuit()) {
         // Clear screen and activate view to render into
